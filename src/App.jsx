8713a6629928e30639e223bd729256971b507295
demo <zsultanali114@gmail.com>
2026-07-07 18:28:48 +0500

    edit link

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Login from './assets/components/login';
 import Register from './assets/components/register';
 import Profile from './assets/components/profile';
 import { ToastContainer } from 'react-toastify';
-import SignupForm from "../../tasklist";
-import EditTask from "../../todolist";
-import { Search } from "../../todolist";
+import SignupForm from "./tasklist";
+import EditTask from "./todolist";
+import { Search } from "./todolist";
 
 
 
