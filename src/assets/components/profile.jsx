542a69da1a0a7b34fcdd59e0e3a2bd08be5ea802
demo <zsultanali114@gmail.com>
2026-07-07 18:14:01 +0500

    remove file

--- src/assets/components/profile.jsx
+++ src/assets/components/profile.jsx
@@ -75,15 +75,7 @@ function Profile() {
     <div>
       {userDetails ? (
         <>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            {userDetails.photo && (
-              <img
-                src={userDetails.photo}
-                width={"40%"}
-                style={{ borderRadius: "50%" }}
-              />
-            )}
-          </div>
+         
           <h3>Welcome {userDetails.firstName}</h3>
           <div>
             <p>Email: {userDetails.email}</p>
