67f9002bfa43a89c44638eedf9531a6f2a3f3db0
demo <zsultanali114@gmail.com>
2026-07-07 13:51:48 +0500

    link comment

--- src/assets/components/login.jsx
+++ src/assets/components/login.jsx
@@ -42,7 +42,7 @@ function Login() {
             <p className="forgot-password text-right">
                 New user <a href="/register">Register Here</a>
             </p>
-            <SignInwithGoogle />
+            {/* <SignInwithGoogle /> */}
         </form>
     )
 }
