60ca0efdd02b480452eefa1fb4bcd7451296ef55
demo <zsultanali114@gmail.com>
2026-07-07 18:22:03 +0500

    add file

--- src/assets/components/profile.jsx
+++ src/assets/components/profile.jsx
@@ -85,9 +85,6 @@ function Profile() {
             <p>Last Name: {userDetails.lastName}</p>
           </div> */}
            <SignupForm />
-
-           <EditTask />
-           <Search />
           {/* <button className="btn btn-primary" onClick={handleLogout}>
             Logout
           </button> */}
