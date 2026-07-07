6e57716f07b7c40b0f5c4dbd25a1f9d9e3ad28d1
demo <zsultanali114@gmail.com>
2026-07-07 15:27:22 +0500

    add profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import  SignupForm  from './tasklist'
-import { Search, EditTask } from './todolist';  
+import SignupForm from './tasklist'
+import { Search, EditTask } from './todolist';
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -13,31 +13,37 @@ import { ToastContainer } from 'react-toastify';
 
 
 export default function App() {
-  return(
-  <>
-  <BrowserRouter>
-  <div className='App'>
-    <div className='auth-wrapper'>
-      <div className='auth-inner'>
-        <Routes>
-        <Route path='/' element ={<Login />} />
-        <Route path='/login' element ={<Login />} />
-        <Route path='/register' element ={<Register />} />
-        <Route path='/profile' element ={<Profile />} />
-        </Routes>
-        <ToastContainer/>
-      </div>
-    </div>
-  </div>
-  </BrowserRouter>
+  const [user, setUser] = useState();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  });
+  return (
+    <>
+      <BrowserRouter>
+        <div className='App'>
+          <div className='auth-wrapper'>
+            <div className='auth-inner'>
+              <Routes>
+                <Route path="/" element={user ? <Navigate to="/profile" /> : <Login />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/profile' element={<Profile />} />
+              </Routes>
+              <ToastContainer />
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
 
-   <BrowserRouter>
-   <Routes>
-     <Route path="/" element={<Search />} />
-     <Route path='/add-task' element={<SignupForm />}/>
-     <Route path='/edit-Task/:id' element={<EditTask />}/>  
-   </Routes>
-   </BrowserRouter>       
-  </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path='/add-task' element={<SignupForm />} />
+          <Route path='/edit-Task/:id' element={<EditTask />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
