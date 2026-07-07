7cac38a4b411f5509fc419718613f63e0c9f5ca3
demo <zsultanali114@gmail.com>
2026-07-07 14:34:14 +0500

    edit error

--- src/assets/components/register.jsx
+++ src/assets/components/register.jsx
@@ -25,12 +25,12 @@ function Register() {
                 });
             }
             console.log("User Registered Successfully!!")
-            toast.error("User Registered Successfully!!", {
+            toast.success("User Registered Successfully!!", {
                 position: "top-center"
             })
         } catch (error) {
             console.log(error.message)
-             toast.success(error.message, {
+             toast.error(error.message, {
                 position: "bottom-center"
             })
         }
