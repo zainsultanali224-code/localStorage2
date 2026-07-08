9152cefd3776762cdf8dc6d5a8d8652e3a6969c5
demo <zsultanali114@gmail.com>
2026-07-08 12:22:34 +0500

    Add image

--- src/assets/components/signInWithGoogle.jsx
+++ src/assets/components/signInWithGoogle.jsx
@@ -1,10 +1,10 @@
-
+import google from "./google.png";
 
 function SignInWithGoogle() {
     return(
         <div>
             <p className="continue-p">--Or continue with--</p>
-           <img src="./assets/components/google.png" alt="Google"  width={"60%"} />
+            <img src={google} alt="Google" width="60%" />
         </div>
     )
 }
