9f0632fe755fcde29990154861e474c09914ef2c
demo <zsultanali114@gmail.com>
2026-07-08 16:07:44 +0500

    full code edit

--- src/assets/components/profile.jsx
+++ src/assets/components/profile.jsx
@@ -1,5 +1,14 @@
-import { useState } from "react";
-import { Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { auth } from "./assets/components/firebase";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './assets/components/login';
+import Register from './assets/components/register';
+import Profile from './assets/components/profile';
+import { ToastContainer } from 'react-toastify';
+import SignupForm from "./tasklist";
+import EditTask from "./todolist";
+import { Search } from "./todolist";
+import Profile_t from './assets/components/profile2';
 
 
 function Profile() {
