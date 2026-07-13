cafc9bdc90efd6ac5dab3d7a5aa9b1ed597a90c9
demo <zsultanali114@gmail.com>
2026-07-13 11:40:34 +0500

    add admin

--- src/assets/components/admin.jsx
+++ src/assets/components/admin.jsx
@@ -1,7 +1,20 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebase";
+
+
 function Admin() {
     return (
         <div>
-            <h1>Admin Dashboard</h1>
+            <h2>User Id</h2>
+            <h3>{user.uid}</h3>
+
+            <h2>User Email</h2>
+            <h3>{user.email}</h3>
+
+            <h2>User Role</h2>
+            <h3>{userDetails.role}</h3>
+
+            
         </div>
     );
 }
