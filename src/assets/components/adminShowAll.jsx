bea5b0316ab82ea903c781836b24bb5e52cdd8c3
demo <zsultanali114@gmail.com>
2026-07-13 12:30:48 +0500

    edit admin

--- src/assets/components/adminShowAll.jsx
+++ src/assets/components/adminShowAll.jsx
@@ -1,8 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './firebase';
+
 function UsersTodos() {
+    const [userTodos, setUserTodos] = useState([]);
+
+    useEffect(() => {
+        const fetchUserTodos = async () => {
+            try {
+                const docSnap = await getDocs(collection(db, "Todos"));
+
+                const todosList = docSnap.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
+                setUserTodos(todosList);
+            } catch (error) {
+                console.error("Error fetching user todos:", error);
+            }
+        };
+
+        fetchUserTodos();
+    }, []);
+
+
     return (
-        <div>
-            <h1>Users Todos</h1>
-        </div>
+       <>
+       <div>
+            <h2>User Todos</h2>
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {userTodos.map((todo, index) => (
+                        <tr key={todo.id}>
+                            <td>{index + 1}</td>
+                            <td>{todo.title}</td>
+                            <td>{todo.description}</td>
+                            <td>{todo.status}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+       </div>
+       </>
     );
 }
 
