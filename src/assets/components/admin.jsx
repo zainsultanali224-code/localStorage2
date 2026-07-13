f61034c7a9239692bd3645e6aa4512b8696fe897
demo <zsultanali114@gmail.com>
2026-07-13 11:59:29 +0500

    add admin

--- src/assets/components/admin.jsx
+++ src/assets/components/admin.jsx
@@ -1,20 +1,54 @@
-import { doc, getDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs, doc } from "firebase/firestore";
 import { db } from "./firebase";
 
 
 function Admin() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    try {
+        const fetchUsers = async () => {
+        const docSnap = await getDocs(collection(db, "Users"))
+
+        const userList = docSnap.docs.map((doc) => ({
+            id: doc.id,
+             ...doc.data()
+               }));
+
+    }
+    setUsers(userList);
+    } catch (error) {
+        console.log(error.message);
+    }
+
+    fetchUsers();
+  }, []);
+  
+
     return (
-        <div>
-            <h2>User Id</h2>
-            <h3>{user.uid}</h3>
+        <div  className="container mt-4">
+          <thead>
+            <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Role</th>
+            </tr>
+          </thead>
 
-            <h2>User Email</h2>
-            <h3>{user.email}</h3>
-
-            <h2>User Role</h2>
-            <h3>{userDetails.role}</h3>
-
-            
+          <tbody>
+            {users.map((user, index) => (
+                <tr key={user.id}>
+                    <td>{index + 1}</td>
+                    <td>{user.firstName}</td>
+                    <td>{user.lastName}</td>
+                    <td>{user.email}</td>
+                    <td>{user.role}</td>
+                </tr>
+            ))}
+          </tbody>
         </div>
     );
 }
