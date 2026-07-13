284bf02a1eb87e5f96538d9e0d68c95bc82ba767
demo <zsultanali114@gmail.com>
2026-07-13 12:12:59 +0500

    edit admin

--- src/assets/components/admin.jsx
+++ src/assets/components/admin.jsx
@@ -1,58 +1,66 @@
 import React, { useEffect, useState } from "react";
 import { collection, getDocs, doc } from "firebase/firestore";
 import { db } from "./firebase";
+import UsersTodos from "./adminShowAll";
+import { navigate } from "react-router-dom";
 
 
 function Admin() {
-  const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-      const fetchUsers = async () => {
+    useEffect(() => {
+        const fetchUsers = async () => {
             try {
-        const docSnap = await getDocs(collection(db, "Users"))
+                const docSnap = await getDocs(collection(db, "Users"))
 
-        const userList = docSnap.docs.map((doc) => ({
-            id: doc.id,
-             ...doc.data()
-               }));
+                const userList = docSnap.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
 
-               setUsers(userList);
+                setUsers(userList);
             } catch (error) {
                 console.error("Error fetching users:", error);
             }
         };
 
-    fetchUsers();
-  }, []);
-  
+        fetchUsers();
+    }, []);
+
 
     return (
-        <div  className="container mt-4">
-            <h2>Users</h2>
-             <table className="table table-bordered">
-          <thead>
-            <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Role</th>
-            </tr>
-          </thead>
+        <>
+            <p onClick={() => {
+                navigate("/adminShowAll");
+            }}> todos</p>
 
-          <tbody>
-            {users.map((user, index) => (
-                <tr key={user.id}>
-                    <td>{index + 1}</td>
-                    <td>{user.firstName}</td>
-                    <td>{user.lastName}</td>
-                    <td>{user.email}</td>
-                    <td>{user.role}</td>
-                </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            <div className="container mt-4">
+                <h2>Users</h2>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {users.map((user, index) => (
+                            <tr key={user.id}>
+                                <td>{index + 1}</td>
+                                <td>{user.firstName}</td>
+                                <td>{user.lastName}</td>
+                                <td>{user.email}</td>
+                                <td>{user.role}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 
---
+++ src/assets/components/adminShowAll.jsx
@@ -0,0 +1,9 @@
+function UsersTodos() {
+    return (
+        <div>
+            <h1>Users Todos</h1>
+        </div>
+    );
+}
+
+export default UsersTodos;
